45cd54afa88c78292e3f6035167f25f796b9eb0c
crsvalle <cristian.valle@pursuit.org>
2023-06-25 16:50:47 -0400

    completed

---
+++ app.js
@@ -0,0 +1,32 @@
+// DEPENDENCIES
+const express = require("express");
+
+// CONFIGURATION
+const app = express();
+const locationsController = require("./controllers/locations.controller.js");
+app.use("/locations", locationsController);
+
+const machinesController = require("./controllers/machines.controller.js");
+app.use("/machines", machinesController);
+
+const personsController = require("./controllers/persons.controller.js");
+app.use("/persons", personsController);
+
+const plansController = require("./controllers/plans.controller.js");
+app.use("/plans", plansController);
+
+const specialEventController = require("./controllers/special-events.controller.js");
+app.use("/special-events", specialEventController);
+
+// ROUTES
+app.get("/", (req, res) => {
+  res.send("Hello, world!");
+});
+
+// 404 PAGE
+app.get("*", (req, res) => {
+    res.status(404),send("Sorry, no page found.")
+  });
+
+// EXPORT
+module.exports = app;
