c58d9a3d5e319468a1afccdef28f22b0cc4c5c3b
Joanavel Pascual <joanavelpascual@pursuit.org>
2023-06-25 17:06:59 -0400

    initial commit

---
+++ app.js
@@ -0,0 +1,26 @@
+// DEPENDENCIES
+const express = require("express");
+
+// CONFIGURATION
+const app = express();
+// const bookmarksController = require("./controllers/bookmarksController.js");
+// app.use("/bookmarks", bookmarksController);
+
+// ROUTES
+app.get("/", (req, res) => {
+  res.send("Hello, world!");
+});
+
+const locationsController = require("./controllers/locations.controller.js");
+app.use("/locations", locationsController.router);
+
+const machinesController = require("./controllers/machines.controller.js");
+app.use("/machines", machinesController.router);
+
+// 404 PAGE
+app.get("*", (req, res) => {
+    res.status(404).json({ error: "Page not found" });
+});
+
+// EXPORT
+module.exports = app;
---
+++ controllers/machines.controller.js
@@ -0,0 +1,8 @@
+const express = require("express");
+const router = express.Router();
+
+// Define your routes and middleware for each controller
+
+module.exports = {
+  router: router
+};
