c6e4b413ebe4365b990a117284384298ffe52f40
Berlin Rivas <berlinrivas@Berlins-MacBook-Air.local>
2023-06-26 13:44:42 -0400

    lab-express-REST-crud-mvc-index done

---
+++ controllers/persons.controller.js
@@ -0,0 +1,9 @@
+const express = require("express")
+const persons = express()
+const personsArray = require("../models/person.model.js")
+
+persons.get("/", (req,res) => {
+    res.json(personsArray)
+})
+
+module.exports = persons
