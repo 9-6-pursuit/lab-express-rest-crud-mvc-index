81e2ef2e434c3e2b88b8bfe3c478fbe882b820fc
atiradoGit88 <angeltirado@pursuit.org>
2023-06-26 17:49:41 -0400

    need to add the proper catch all errors page.

---
+++ controllers/plans.controller.js
@@ -0,0 +1,9 @@
+const express = require ("express");
+const plans = express();
+const plansArray = require("../models/plan.model.js")
+
+plans.get("/", (req, res) => {
+    res.json(plansArray);
+});
+
+module.exports = plans
