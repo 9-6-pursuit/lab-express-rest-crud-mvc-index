87faee21fdabaea40be359a3864daaa1484de6cc
Lakisha J <lakishajohnson@pursuit.org>
2023-06-27 15:54:51 -0400

    This is as far as I could get for the BONUS

--- app.js
+++ app.js
@@ -1,12 +1,13 @@
 const express = require("express");
+const app = express();
 
 const locationsController = require("./controllers/locations.controller");
 const peopleController = require("./controllers/persons.controller");
 const plansController = require("./controllers/plans.controller");
 const equipmentController = require("./controllers/machines.controller");
 const specialEventController = require("./controllers/special-events.controller");
-
-const app = express();
+const locations = require("./models/location.model");
+const people = require("./models/person.model");
 
 app.use(express.json());
 
@@ -19,6 +20,29 @@ app.use("/special-events", specialEventController);
 
 app.get("/", (req, res) => {
   res.send("Hello, world!");
+});
+
+app.get("/locations/people", (req, res) => {
+  const output = locations.map((location) => {
+    if (location.zip === "11011") {
+      const members = people.filter(
+        (person) => person.mainLocation !== location.zip
+      );
+      return { ...location, members };
+    } else {
+      const peopleAtLocation = people.filter(
+        (person) => person.mainLocation === location.zip
+      );
+      return { ...location, people: peopleAtLocation };
+    }
+  });
+  res.json(output);
+}); 
+
+
+// 404 PAGE ROUTE
+app.get("*", (req, res) => {
+  res.status(404).json({ error: "Sorry, no page found!" });
 });
 
 module.exports = app;
