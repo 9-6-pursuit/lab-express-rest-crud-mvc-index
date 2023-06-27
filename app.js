a2fc56b1d47158ed85e5fa1b27d08a5810956c56
Lakisha J <lakishajohnson@pursuit.org>
2023-06-27 12:15:15 -0400

    Required the rest of the controllers

--- app.js
+++ app.js
@@ -1,4 +1,9 @@
 const express = require("express");
+
+const locationsController = require("./controllers/locations.controller");
+const peopleController = require("./controllers/persons.controller");
+const plansController = require("./controllers/plans.controller");
+const equipmentController = require("./controllers/machines.controller");
 const specialEventController = require("./controllers/special-events.controller");
 
 const app = express();
@@ -6,6 +11,10 @@ const app = express();
 app.use(express.json());
 
 // Use specialEventController as middleware for the "/locations" route
+app.use("/locations", locationsController);
+app.use("/people", peopleController);
+app.use("/plans", plansController);
+app.use("/equipment", equipmentController);
 app.use("/special-events", specialEventController);
 
 app.get("/", (req, res) => {
