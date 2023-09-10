bcc3889ec7b6f55cc179329a33c55f892cbbae1c
Antoine M. Shepard <antoineshepard@pursuit.org>
2023-09-10 15:14:19 -0400

    Initial commit.

---
+++ app.js
@@ -0,0 +1,20 @@
+// DEPENDENCIES
+const express = require("express");
+
+// CONFIGURATION
+const app = express();
+const bookmarksController = require("./controllers/bookmarksController.js");
+app.use("/bookmarks", bookmarksController);
+
+// ROUTES
+app.get("/", (req, res) => {
+  res.send("Welcome to Bookmarks App");
+});
+
+// 404 PAGE
+app.get("*", (req, res) => {
+    res.status(404).json({ error: "Page not found" });
+  });
+
+// EXPORT
+module.exports = app;
